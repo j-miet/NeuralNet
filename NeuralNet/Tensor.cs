3b2d51dd770a4e15549a272b09592e367469434d
j-miet <191247923+j-miet@users.noreply.github.com>
2025-04-09 22:54:22 +0300

    Update Tensor class

--- NeuralNet/Tensor.cs
+++ NeuralNet/Tensor.cs
@@ -1,40 +1,39 @@
-/*Tensor implementation
-*/
+
 
 namespace NeuralNet{
 
     /// <summary>
-    /// Tensor class for 1D arrays
+    /// Tensor class for 2D arrays
     /// </summary>
     class Tensor
     {
-        private readonly double[] data;
+        private readonly double[,] data;
 
         /// <summary>
-        /// Initializes a new tensor object
+        /// Initializes a new Tensor
         /// </summary>
-        /// <param name="data">Data array of double type values</param>      
-        public Tensor(double[] data)
+        /// <param name="data">Data matrix of double type values</param>      
+        public Tensor(double[,] data)
         {
             this.data = data;
         }
 
         /// <summary>
-        /// Returns data array
+        /// Returns data matrix
         /// </summary>
-        /// <returns>Data array of all values</returns> 
-        public double[] GetData()
+        /// <returns>Data matrix of all values</returns> 
+        public double[,] GetData()
         {
             return data;
         }
 
         /// <summary>
-        /// Returns size of tensor
+        /// Returns size of tensor as an array [rows, columns]
         /// </summary>
-        /// <returns>Size of data array</returns>
-        public int GetSize()
+        /// <returns>Size of data matrix</returns>
+        public int[] GetSize()
         {
-            return data.Length;
+            return [data.GetLength(0), data.GetLength(1)];
         }
 
         /// <summary>
@@ -42,33 +41,45 @@ namespace NeuralNet{
         /// </summary>  
         public void PrintData()
         {
-            int size = data.Length;
-            Console.WriteLine("Size: " + size + ", ["+string.Join<double>(" ", data)+"]");
+            double[] rowValues = new double[data.GetLength(1)];
+            Console.WriteLine("Size: ("+ GetSize()[0]+", "+GetSize()[1]+")");
+            for (int row = 0; row < data.GetLength(0); row++)
+            {
+                for (int col = 0; col < data.GetLength(1); col++)
+                {
+                    rowValues[col] = data[row,col];
+                }
+                Console.WriteLine("Row " + row +": ["+string.Join<double>(" ", rowValues)+"]");
+            }
         }
 
         /// <summary>
         /// Sums tensor1 and tensor2 elementwise and returns this new tensor
         /// </summary>
-        /// <param name="tensor1">Tensor object</param>
-        /// <param name="tensor2">Tensor object</param>
-        /// <returns>Tensor object</returns>
+        /// <param name="tensor1">Tensor</param>
+        /// <param name="tensor2">Tensor</param>
+        /// <returns>Tensor</returns>
         /// <exception cref="Exception">Tensors have different size</exception>
         public static Tensor SumTensors(Tensor tensor1, Tensor tensor2)
         {
-            if (tensor1.data.Length != tensor2.data.Length)
+            int[] tensor1Size = tensor1.GetSize();
+            int[] tensor2Size = tensor2.GetSize();
+            
+            if (tensor1Size[0] != tensor2Size[0] || tensor1Size[1] != tensor2Size[1])
             {
                 throw new Exception("Size of tensors don't match!");
             }
             else
             {
-                double[] data1 = new double[tensor1.data.Length];
-                Array.Copy(tensor1.data, data1, tensor1.data.Length);
-                double[] data2 = new double[tensor2.data.Length];
-                Array.Copy(tensor2.data, data2, tensor2.data.Length);
+                double[,] data1 = (double[,])tensor1.data.Clone();
+                double[,] data2 = (double[,])tensor2.data.Clone();
 
-                for (int i = 0; i < data1.Length; i++)
+                for (int row = 0; row < data1.GetLength(0); row++)
                 {
-                    data1[i] += data2[i];
+                    for (int col = 0; col < data1.GetLength(1); col++)
+                    {
+                        data1[row,col] += data2[row,col];
+                    }
                 }
                 Tensor tensorObject = new(data1);
                 return tensorObject;
@@ -78,15 +89,18 @@ namespace NeuralNet{
         /// <summary>
         /// Multiplies all tensor values by -1 and returns this new tensor.
         /// </summary>
-        /// <param name="tensor">Tensor object</param>
-        /// <returns>Tensor object</returns>
+        /// <param name="tensor">Tensor</param>
+        /// <returns>Tensor</returns>
         public static Tensor MinusTensor(Tensor tensor)
         {
-            double[] data = new double[tensor.data.Length];
-            Array.Copy(tensor.data, data, tensor.data.Length);
-            for (int i = 0; i < tensor.data.Length; i++)
+            double[,] data = (double[,])tensor.data.Clone();
+
+            for (int row = 0; row < data.GetLength(0); row++)
             {
-               data[i] = -data[i];
+                for (int col = 0; col < data.GetLength(1); col++)
+                {
+                    data[row,col] = -data[row,col];
+                }
             }
             Tensor tensorObject = new(data);
             return tensorObject;
@@ -95,31 +109,32 @@ namespace NeuralNet{
         /// <summary>
         /// Substracts tensor1 from tensor2 elementwise and return this new tensor
         /// </summary>
-        /// <param name="tensor1">Tensor object</param>
-        /// <param name="tensor2">Tensor object</param>
-        /// <returns>Tensor object</returns>
+        /// <param name="tensor1">Tensor</param>
+        /// <param name="tensor2">Tensor</param>
+        /// <returns>Tensor</returns>
         public static Tensor SubtractTensors(Tensor tensor1, Tensor tensor2)
         {
-            return Tensor.SumTensors(tensor1, Tensor.MinusTensor(tensor2));
+            return SumTensors(tensor1, MinusTensor(tensor2));
         }
 
         /// <summary>
         /// Returns a scaled tensor
         /// Each value is scaled multiplier, scalar*tensor[i]
         /// </summary>
-        /// <param name="tensor">Tensor object</param>
+        /// <param name="tensor">Tensor</param>
         /// <param name="scalar">Scalar value</param>
-        /// <returns>Tensor object</returns>   
+        /// <returns>Tensor</returns>   
         public static Tensor ScaleTensor(Tensor tensor, double scalar)
         {
-            double[] data = new double[tensor.data.Length];
-            Array.Copy(tensor.data, data, tensor.data.Length);
+            double[,] data = (double[,])tensor.data.Clone();
 
-            for (int i = 0; i < data.Length; i++)
+            for (int row = 0; row < data.GetLength(0); row++)
             {
-                data[i] *= scalar;
+                for (int col = 0; col < data.GetLength(1); col++)
+                {
+                    data[row,col] *= scalar;
+                }
             }
-
             Tensor tensorObject = new(data);
             return tensorObject;
         }
@@ -127,29 +142,33 @@ namespace NeuralNet{
         /// <summary>
         /// Multiplies two tensors elementwise and returns this product tensor.
         /// </summary>
-        /// <param name="tensor1">Tensor object</param>
-        /// <param name="tensor2">Tensor object</param>
-        /// <returns>Tensor object</returns>
+        /// <param name="tensor1">Tensor</param>
+        /// <param name="tensor2">Tensor</param>
+        /// <returns>Tensor</returns>
         /// <exception cref="Exception">Tensors have different size</exception>
         public static Tensor MultiplyTensor(Tensor tensor1, Tensor tensor2)
         {
-            if (tensor1.data.Length != tensor2.data.Length)
+            int[] tensor1Size = tensor1.GetSize();
+            int[] tensor2Size = tensor2.GetSize();
+            
+            if (tensor1Size[0] != tensor2Size[0] || tensor1Size[1] != tensor2Size[1])
             {
                 throw new Exception("Size of tensors don't match!");
             }
             else
             {
-                double[] data1 = new double[tensor1.data.Length];
-                Array.Copy(tensor1.data, data1, tensor1.data.Length);
-                double[] data2 = new double[tensor2.data.Length];
-                Array.Copy(tensor2.data, data2, tensor2.data.Length);
+                double[,] data1 = (double[,])tensor1.data.Clone();
+                double[,] data2 = (double[,])tensor2.data.Clone();
 
-                for (int i = 0; i < data1.Length; i++)
+                    for (int row = 0; row < data1.GetLength(0); row++)
                 {
-                    data1[i] *= data2[i];
+                    for (int col = 0; col < data1.GetLength(1); col++)
+                    {
+                        data1[row,col] *= data2[row,col];
+                    }
                 }
-                Tensor tensorObject = new(data1);
-                return tensorObject;
+                    Tensor tensorObject = new(data1);
+                    return tensorObject;
             }
 
         }
@@ -157,7 +176,7 @@ namespace NeuralNet{
         /// <summary>
         /// Sums all values of a tensor
         /// </summary>
-        /// <param name="tensor">Tensor Object</param>
+        /// <param name="tensor">Tensor</param>
         /// <returns>Sum of values</returns>
         public static double SumValues(Tensor tensor)
         {
@@ -170,27 +189,94 @@ namespace NeuralNet{
         }
 
         /// <summary>
-        /// Calculates the dot product of two tensors
+        /// Calculates the product tensor
         /// </summary>
-        /// <param name="tensor1">Tensor object</param>
-        /// <param name="tensor2">Tensor object</param>
-        /// <returns>Dot product</returns>
-        /// <exception cref="Exception">Tensors have different size</exception>
-        public static double DotProduct(Tensor tensor1, Tensor tensor2)
+        /// <param name="tensor1">Tensor</param>
+        /// <param name="tensor2">Tensor</param>
+        /// <returns>Tensor</returns>
+        /// <exception cref="Exception">tensor1 columns and tensor2 rows don't match in size</exception>
+        public static Tensor TensorProduct(Tensor tensor1, Tensor tensor2)
         {
-            if (tensor1.data.Length != tensor2.data.Length)
+            int tensor1Cols = tensor1.GetSize()[1];
+            int tensor2Rows = tensor2.GetSize()[0];
+            int tensor1Rows = tensor1.GetSize()[0];
+            int tensor2Cols = tensor2.GetSize()[1];
+
+            if (tensor1Cols != tensor2Rows)
             {
-                throw new Exception("Size of tensors don't match!");
-            }
-            else
-            {
-                double dotProduct = 0; 
-                for (int i = 0; i < tensor1.data.Length; i++)
+                if (tensor1Cols == 1 || tensor1Rows == 1 && tensor2Rows == 1 && tensor2Cols == 1)
                 {
-                    dotProduct += tensor1.data[i]*tensor2.data[i];
+                    // dot product
+                    tensor2 = Transpose(tensor2);
+                    tensor2Rows = tensor2.GetSize()[0];
+                    tensor2Cols = tensor2.GetSize()[1];
                 }
-                return dotProduct;
+                else
+                {
+                    throw new Exception("tensor1 columns and tensor2 rows don't match");
+                } 
             }
+            double[,] data1 = (double[,])tensor1.data.Clone();
+            double[,] data2 = (double[,])tensor2.data.Clone();
+
+            double[,] productTensor = new double[tensor1Rows, tensor2Cols];
+            for (int i = 0; i < tensor1Rows; i++)
+            {
+                for (int j = 0; j < tensor2Cols; j++)
+                {
+                    for (int k = 0; k < tensor2Rows; k++)
+                    {
+                        productTensor[i, j] += data1[i, k] * data2[k, j];
+                    } 
+                }
+            }
+            Tensor tensorObject = new(productTensor);
+            return tensorObject;
+        }
+
+        /// <summary>
+        /// Returns transposed tensor
+        /// </summary>
+        /// <param name="tensor">Tensor</param>
+        /// <returns>Tensor</returns>
+        public static Tensor Transpose(Tensor tensor)
+        {
+            int rowSize = tensor.GetSize()[0];
+            int colSize = tensor.GetSize()[1];
+
+            double[,] tensorArray = new double[colSize,rowSize];
+            double[,] data = (double[,])tensor.data.Clone();
+
+            for (int i = 0; i < rowSize; i++)
+            {
+                for (int j = 0; j < colSize; j++)
+                {
+                    tensorArray[j, i] = data[i ,j];
+                }
+            }
+            Tensor tensorObject = new(tensorArray);
+            return tensorObject;
+        }
+
+        /// <summary>
+        /// Returns an array of rows from tensor, each as 1xn tensor object
+        /// </summary>
+        /// <param name="tensor">Tensor</param>
+        /// <returns>Array of tensors</returns>
+        public static Tensor[] TensorList(Tensor tensor)
+        {
+            double[,] tensorData = (double[,])tensor.GetData().Clone();
+            Tensor[] tensorRows = new Tensor[tensor.GetSize()[0]];
+            for (int row = 0; row < tensor.GetSize()[0]; row++)
+            {
+                double[,] tempRow = new double[tensor.GetSize()[1], 1];
+                for (int col = 0; col < tensor.GetSize()[1]; col++)
+                {
+                    tempRow[col,0] = tensorData[row, col];
+                }
+                tensorRows[row] = new Tensor(tempRow);
+            }
+            return tensorRows;
         }
     }
 }
