7cbc56747a2c1c63911ea60dc1419d72775432db
j-miet <191247923+j-miet@users.noreply.github.com>
2025-04-09 22:48:15 +0300

    Implement NN class

--- NeuralNet/Network.cs
+++ NeuralNet/Network.cs
@@ -1,18 +1,55 @@
 ﻿namespace NeuralNet
 {
-    
+    /// <summary>
+    /// Neural network class
+    /// </summary>
     class NN
     {
-        private readonly int inCount;
-        private readonly int outCount;
-        private readonly int layers;
+        private Layer[] layers;
 
-        public NN(int inCount, int outCount, int layers)
+        public NN(Layer[] layers)
         {
-            this.inCount = inCount;
-            this.outCount = outCount;
             this.layers = layers;
         }
-    }
-}
 
+        /// <summary>
+        /// Forwards entire network
+        /// </summary>
+        /// <param name="inputs">Input layer data</param>
+        /// <returns></returns>
+        public Tensor Forward(Tensor inputs)
+        {
+            Tensor outputValues = inputs;
+            foreach (Layer layer in layers)
+            {
+                outputValues = layer.Forward(outputValues);
+            }
+            return outputValues;
+        }
+
+        /// <summary>
+        /// Backpropagates through all layers
+        /// </summary>
+        /// <param name="gradient">Loss function gradient</param>
+        /// <returns>Gradient of first layer (if needed for some reason)</returns>
+        public Tensor Backward(Tensor gradient)
+        {
+            Tensor gradValues = gradient;
+            foreach(Layer layer in layers.Reverse())
+            {
+                gradValues = layer.Backward(gradValues);
+            }
+            return gradValues;
+        }
+
+        /// <summary>
+        /// Returns an array of all layers
+        /// </summary>
+        /// <returns>Array of Layer objects</returns>
+        public Layer[] GetLayers()
+        {
+            return layers;
+        }
+    }
+    
+}
